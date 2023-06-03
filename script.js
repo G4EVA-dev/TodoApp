6a6f7d3027f07b5c3e02db64a4c98644a37b4cfd
G4EVA-dev <tanzeglenn@gmail.com>
2023-06-03 02:20:44 +0100

    change background color and add hover to botton

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ listContainer.addEventListener(
   function (e) {
     if (e.target.tagName === "LI") {
       e.target.classList.toggle("checked");
-      saveSdata();
+      savedata();
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
       savedata();
